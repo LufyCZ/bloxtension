300cbc7b06729bb3345650b9cb05bc20d099662c
Vojtch159 <vojta159.studeny@gmail.com>
2025-05-30 22:36:21 +0200

    style: add blockscout logo

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Settings from "~settings"
 import SettingsIcon from "data-base64:./../assets/settings-icon.svg"
+import BlockscoutIcon from "data-base64:./../assets/blockscout.svg"
 import "~style.css"
 
 function IndexPopup() {
@@ -8,10 +9,15 @@ function IndexPopup() {
 
   return (
     <div className="flex flex-col w-64">
+      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-start">
+        <img src={BlockscoutIcon} alt="Blockscout" />
+      </div>
       <div className="flex flex-row justify-end">
       <button onClick={() => setShowSettings(!showSettings)}>
         <img src={SettingsIcon} alt="Settings" />
       </button>
+      </div>
       </div>
       {showSettings && <Settings />}
     </div>
