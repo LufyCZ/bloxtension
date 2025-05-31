693dbb40a580a4fd7fe16fd1e7bad07943ed285e
LufyCZ <me@lufy.cz>
2025-05-31 15:05:37 +0200

    Fix Settings Overflow

--- src/settings.tsx
+++ src/settings.tsx
@@ -13,8 +13,8 @@ function Settings() {
     const [showDetailSettings, setShowDetailSettings] = useState(false)
 
     if (showChains) {
-        return (
-            <div className="flex flex-col gap-2 pl-4">
+                                                                                                                                                                                                                                                                                                                return (
+            <div className="flex flex-col gap-2 pl-4 overflow-x-hidden">
                 <button className="flex justify-end text-sm pr-2" onClick={() => setShowChains(false)}>back</button>
                 <div className="flex flex-row gap-2">
                     <h3 className="text-lg">Chains</h3>
