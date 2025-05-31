907e32c1cb2b0841a4bb64625842b1dba9fcfc8b
Vojtch159 <vojta159.studeny@gmail.com>
2025-05-31 11:38:48 +0200

    style: settings

--- src/popup.tsx
+++ src/popup.tsx
@@ -8,16 +8,16 @@ function IndexPopup() {
   const [showSettings, setShowSettings] = useState(false)
 
   return (
-    <div className="flex flex-col w-64">
+    <div className="flex flex-col w-96 h-64">
       <div className="flex flex-row justify-between">
-      <div className="flex flex-row justify-start">
-        <img src={BlockscoutIcon} alt="Blockscout" />
-      </div>
-      <div className="flex flex-row justify-end">
-      <button onClick={() => setShowSettings(!showSettings)}>
-        <img src={SettingsIcon} alt="Settings" />
-      </button>
-      </div>
+        <div className="flex flex-row justify-start p-2">
+          <img src={BlockscoutIcon} alt="Blockscout" />
+        </div>
+        <div className="flex flex-row justify-end p-2">
+          <button onClick={() => setShowSettings(!showSettings)}>
+            <img src={SettingsIcon} alt="Settings" />
+          </button>
+        </div>
       </div>
       {showSettings && <Settings />}
     </div>
